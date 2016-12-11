73c3cffbedef3cce8ad7fe2bab1040f432a5cbb2
K10shah <ketan.shah2014@gmail.com>
2016-12-12 02:29:15 +0530

    Initial commit

---
+++ WindowsApplication1/CalcGUI/MainPage.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainPage
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FirstDigit = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Oper = New System.Windows.Forms.Label()
+        Me.SecondDigit = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LabelAns = New System.Windows.Forms.Label()
+        Me.ButtonReset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'FirstDigit
+        '
+        Me.FirstDigit.AllowDrop = True
+        Me.FirstDigit.AutoSize = True
+        Me.FirstDigit.Font = New System.Drawing.Font("Arial", 69.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FirstDigit.Location = New System.Drawing.Point(24, 62)
+        Me.FirstDigit.Name = "FirstDigit"
+        Me.FirstDigit.Size = New System.Drawing.Size(97, 108)
+        Me.FirstDigit.TabIndex = 0
+        Me.FirstDigit.Text = "0"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(106, 173)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(83, 26)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Equals"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Oper
+        '
+        Me.Oper.AutoSize = True
+        Me.Oper.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Oper.Location = New System.Drawing.Point(110, 77)
+        Me.Oper.Name = "Oper"
+        Me.Oper.Size = New System.Drawing.Size(71, 76)
+        Me.Oper.TabIndex = 3
+        Me.Oper.Text = "?"
+        '
+        'SecondDigit
+        '
+        Me.SecondDigit.AllowDrop = True
+        Me.SecondDigit.AutoSize = True
+        Me.SecondDigit.Font = New System.Drawing.Font("Arial", 69.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SecondDigit.Location = New System.Drawing.Point(173, 62)
+        Me.SecondDigit.Name = "SecondDigit"
+        Me.SecondDigit.Size = New System.Drawing.Size(97, 108)
+        Me.SecondDigit.TabIndex = 4
+        Me.SecondDigit.Text = "0"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(22, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(259, 25)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Voice guided calculator"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(18, 216)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(103, 25)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Answer: "
+        '
+        'LabelAns
+        '
+        Me.LabelAns.AutoSize = True
+        Me.LabelAns.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelAns.Location = New System.Drawing.Point(127, 216)
+        Me.LabelAns.Name = "LabelAns"
+        Me.LabelAns.Size = New System.Drawing.Size(0, 25)
+        Me.LabelAns.TabIndex = 7
+        '
+        'ButtonReset
+        '
+        Me.ButtonReset.Location = New System.Drawing.Point(25, 260)
+        Me.ButtonReset.Name = "ButtonReset"
+        Me.ButtonReset.Size = New System.Drawing.Size(81, 24)
+        Me.ButtonReset.TabIndex = 8
+        Me.ButtonReset.Text = "Reset"
+        Me.ButtonReset.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(293, 308)
+        Me.Controls.Add(Me.ButtonReset)
+        Me.Controls.Add(Me.LabelAns)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.SecondDigit)
+        Me.Controls.Add(Me.Oper)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.FirstDigit)
+        Me.Name = "Form1"
+        Me.Text = "Voice guided calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents FirstDigit As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Oper As System.Windows.Forms.Label
+    Friend WithEvents SecondDigit As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents LabelAns As System.Windows.Forms.Label
+    Friend WithEvents ButtonReset As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication1/CalcGUI/MainPage.vb
@@ -0,0 +1,227 @@
+﻿Imports System.Speech
+
+Public Class MainPage
+
+    Dim WithEvents reco As New Recognition.SpeechRecognitionEngine
+    Dim first As Integer = 0
+    Dim firstFlag As Boolean = False
+    Dim second As Integer = 0
+    Dim secondFlag As Boolean = False
+    Dim sign As String = ""
+    Dim signFlag As Boolean = False
+    Dim choice As String = ""
+    Private Sub SetColor(ByVal color As System.Drawing.Color)
+
+        'Dim synth As New Synthesis.SpeechSynthesizer
+
+        'synth.SpeakAsync("setting the back color to " + color.ToString)
+
+        Me.BackColor = color
+
+    End Sub
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Button1.Visible = False
+        reco.SetInputToDefaultAudioDevice()
+
+        Dim gram As New Recognition.SrgsGrammar.SrgsDocument
+
+        Dim colorRule As New Recognition.SrgsGrammar.SrgsRule("color")
+
+        Dim colorsList As New Recognition.SrgsGrammar.SrgsOneOf("1", "2", "3", "4", "5", "6", "7", "9", "0", "+", "minus", "divide", "*", "multiply", "plus", "add", "subtract", "divided by", "multiplied by", "into")
+
+        colorRule.Add(colorsList)
+
+        gram.Rules.Add(colorRule)
+
+        gram.Root = colorRule
+
+        reco.LoadGrammar(New Recognition.Grammar(gram))
+
+        reco.RecognizeAsync()
+
+    End Sub
+
+    Private Sub reco_RecognizeCompleted(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognizeCompletedEventArgs) Handles reco.RecognizeCompleted
+
+        reco.RecognizeAsync()
+
+    End Sub
+
+    Private Sub reco_SpeechRecognized(ByVal sender As Object, ByVal e As System.Speech.Recognition.RecognitionEventArgs) Handles reco.SpeechRecognized
+        Console.Write(e.Result.Text)
+        choice = e.Result.Text
+        If firstFlag = False Then
+            Select Case choice
+                Case "0"
+                    FirstDigit.Text = 0
+                    first = 0
+                    firstFlag = True
+                Case "1"
+                    FirstDigit.Text = 1
+                    first = 1
+                    firstFlag = True
+                Case "2"
+                    FirstDigit.Text = 2
+                    first = 2
+                    firstFlag = True
+                Case "3"
+                    FirstDigit.Text = 3
+                    first = 3
+                    firstFlag = True
+                Case "4"
+                    FirstDigit.Text = 4
+                    first = 4
+                    firstFlag = True
+                Case "5"
+                    FirstDigit.Text = 5
+                    first = 5
+                    firstFlag = True
+                Case "6"
+                    FirstDigit.Text = 6
+                    first = 6
+                    firstFlag = True
+                Case "7"
+                    FirstDigit.Text = 7
+                    first = 7
+                    firstFlag = True
+                Case "8"
+                    FirstDigit.Text = 8
+                    first = 8
+                    firstFlag = True
+                Case "9"
+                    FirstDigit.Text = 9
+                    first = 9
+                    firstFlag = True
+            End Select
+
+
+        ElseIf signFlag = False Then
+            Select Case choice
+                Case "plus"
+                    Oper.Text = "+"
+                    sign = "+"
+                    signFlag = True
+                Case "add"
+                    Oper.Text = "+"
+                    sign = "+"
+                    signFlag = True
+                Case "divide"
+                    Oper.Text = "/"
+                    sign = "/"
+                    signFlag = True
+                Case "divided by"
+                    Oper.Text = "/"
+                    sign = "/"
+                    signFlag = True
+                Case "minus"
+                    Oper.Text = "-"
+                    sign = "-"
+                    signFlag = True
+                Case "subtract"
+                    Oper.Text = "-"
+                    sign = "-"
+                    signFlag = True
+                Case "*"
+                    Oper.Text = "X"
+                    sign = "X"
+                    signFlag = True
+                Case "multiplied by"
+                    Oper.Text = "X"
+                    sign = "X"
+                    signFlag = True
+                Case "into"
+                    Oper.Text = "X"
+                    sign = "X"
+                    signFlag = True
+                Case "multiply"
+                    Oper.Text = "X"
+                    sign = "X"
+                    signFlag = True
+            End Select
+        ElseIf secondFlag = False Then
+            Select Case choice
+                Case "0"
+                    SecondDigit.Text = 0
+                    second = 0
+                    secondFlag = True
+                Case "1"
+                    SecondDigit.Text = 1
+                    second = 1
+                    secondFlag = True
+                Case "2"
+                    SecondDigit.Text = 2
+                    second = 2
+                    secondFlag = True
+                Case "3"
+                    SecondDigit.Text = 3
+                    second = 3
+                    secondFlag = True
+                Case "4"
+                    SecondDigit.Text = 4
+                    second = 4
+                    secondFlag = True
+                Case "5"
+                    SecondDigit.Text = 5
+                    second = 5
+                    secondFlag = True
+                Case "6"
+                    SecondDigit.Text = 6
+                    second = 6
+                    secondFlag = True
+                Case "7"
+                    SecondDigit.Text = 7
+                    second = 7
+                    secondFlag = True
+                Case "8"
+                    SecondDigit.Text = 8
+                    second = 8
+                    secondFlag = True
+                Case "9"
+                    SecondDigit.Text = 9
+                    second = 9
+                    secondFlag = True
+            End Select
+            Button1.Visible = True
+            Button1.Enabled = True
+        End If
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Select Case sign
+            Case "+"
+                LabelAns.Text = (first + second).ToString()
+            Case "-"
+                LabelAns.Text = (first - second).ToString()
+            Case "/"
+                If second = 0 Then
+                    LabelAns.Text = "Invalid input"
+                Else
+                    LabelAns.Text = (first / second).ToString()
+                End If
+            Case "X"
+                LabelAns.Text = (first * second).ToString()
+        End Select
+        Button1.Visible = False
+        Button1.Enabled = False
+    End Sub
+
+    Private Sub ButtonReset_Click(sender As System.Object, e As System.EventArgs) Handles ButtonReset.Click
+        first = 0
+        firstFlag = False
+        FirstDigit.Text = "0"
+        second = 0
+        secondFlag = False
+        SecondDigit.Text = "0"
+        Oper.Text = "?"
+        sign = ""
+        signFlag = False
+        Button1.Visible = False
+        Button1.Enabled = False
+        LabelAns.Text = ""
+    End Sub
+End Class
